b78373887756398d97a4b337721d82f20b7a062c
guilhembn <gbuisan@laas.fr>
2020-11-27 18:28:54 +0100

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='arucolvert',
+    version='1.0.0',
+    packages=['arucolvert'],
+    url='https://github.com/rustyducks/arucol-vert-python',
+    license='MIT',
+    author='gbuisan',
+    author_email='buisanguilhem@gmail.com',
+    description=''
+)
